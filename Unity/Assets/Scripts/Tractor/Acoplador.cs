b8633b96a24f1d2b8c1d36c2a211fa30214ce705
santi-17 <santiagosibilia1998@gmail.com>
2025-06-22 19:04:51 -0300

    Se hace el script del acople y el desacople

---
+++ Unity/Assets/Scripts/Tractor/Acoplador.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acoplador : MonoBehaviour
+{
+    public KeyCode teclaAcoplar = KeyCode.E; // Tecla para acoplar/desacoplar
+    public KeyCode teclaDesacoplar = KeyCode.Q; // Tecla para desacoplar
+
+    private Implemento implementoActual = null; // Referencia al implemento actual acoplado
+    private Implemento implementoCercano = null;
+    
+    private Transform puntoAcople; // Transform del punto de acople del tractor
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        // Verifica si el objeto con el que colisiona es un implemento
+        Implemento implemento = other.GetComponent<Implemento>();
+        if (implemento != null && !implemento.esActivo)
+        {
+            implementoCercano = implemento; // Guarda el implemento cercano
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Verifica si el objeto que sale del trigger es el implemento cercano
+        if (implementoCercano != null && other.GetComponent<Implemento>() == implementoCercano)
+        {
+            implementoCercano = null; // Limpia la referencia al implemento cercano
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(teclaAcoplar) && implementoCercano != null && implementoActual == null)
+        {
+            // Si se presiona la tecla de acoplar y hay un implemento cercano, acopla el implemento
+            implementoCercano.Acoplar(transform); // Acopla el implemento al punto de acople del tractor
+            implementoActual = implementoCercano; // Guarda el implemento actual
+            implementoCercano = null; // Limpia la referencia al implemento cercano
+        }
+        else if (Input.GetKeyDown(teclaDesacoplar) && implementoActual != null)
+        {
+            // Si se presiona la tecla de desacoplar y hay un implemento acoplado, desacopla el implemento
+            implementoActual.Desacoplar(); // Desacopla el implemento del tractor
+            implementoActual = null; // Limpia la referencia al implemento actual
+        }
+    }
+}
